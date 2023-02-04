a8ae564a240ee77a3759b94360cbbec3b984774c
Bagohart <bagohart@gmx.de>
2023-02-04 22:38:11 +0100

    try this

---
+++ README.md
@@ -0,0 +1,2 @@
+# niceblock.nvim
+bla
---
+++ plugin/niceblock.lua
@@ -0,0 +1,11 @@
+vim.keymap.set("x", "<Plug>(nvim-niceblock-I)", function()
+	return require("niceblock").nice_block_I()
+end, { expr = true })
+
+vim.keymap.set("x", "<Plug>(nvim-niceblock-A)", function()
+	return require("niceblock").nice_block_A()
+end, { expr = true })
+
+vim.keymap.set("x", "<Plug>(nvim-niceblock-gI)", function()
+	return require("niceblock").nice_block_gI()
+end, { expr = true })
